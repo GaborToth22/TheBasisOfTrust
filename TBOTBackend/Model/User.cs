55bb23f6901e83980afd1467fd69bb6445624e50
GaborToth22 <toccs22@gmail.com>
2024-04-21 10:38:28 +0200

    feat: Create Model Directory and add User model

--- TBOTBackend/Model/User.cs
+++ TBOTBackend/Model/User.cs
@@ -2,13 +2,10 @@ using Microsoft.AspNetCore.Identity;
 
 namespace TBOTBackend.Model;
 
-public class User : IdentityUser
+public class User 
 {
+    public int Id { get; init; }
     public string Username { get; set; }
     public string Email { get; set; }
-    public string PhoneNumber { get; set; }
-    public IList<User> Friends { get; set; }
-    public IList<Expense> Expenses { get; set; }
-    public IList<SettleUp> SettleUps { get; set; }
-
+    public ICollection<Friendship> Friendships { get; set; }
 }
