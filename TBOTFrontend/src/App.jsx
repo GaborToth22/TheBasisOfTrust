3102e531f79bf60f6b83272dd07962d8cb68304f
GaborToth22 <toccs22@gmail.com>
2024-04-23 08:31:37 +0200

    feat: import react-router-dom

--- TBOTFrontend/src/App.jsx
+++ TBOTFrontend/src/App.jsx
@@ -1,48 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Container, Row, Col  } from 'react-bootstrap';
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <Button variant="primary">Primary Button</Button>
-        <Container>
-      <Row>
-        <Col xs={12} md={6}>
-          Első oszlop
-        </Col>
-        <Col xs={12} md={6}>
-          Második oszlop
-        </Col>
-      </Row>
-    </Container>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      
+    </Router>
+  );
 }
 
-export default App
+export default App;
