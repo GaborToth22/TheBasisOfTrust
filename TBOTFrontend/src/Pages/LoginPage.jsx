4559eef2fad652208a141b95f82cd316d7555312
GaborToth22 <toccs22@gmail.com>
2024-04-24 13:12:46 +0200

    refactor: fixed LoginPage and RegistrationModal

--- TBOTFrontend/src/Pages/LoginPage.jsx
+++ TBOTFrontend/src/Pages/LoginPage.jsx
@@ -26,7 +26,7 @@ function LoginPage({loggedUser, setLoggedUser}){
             });            
 
             if (response.ok ) {
-                const response = await fetch(`/users/${username}`);
+                const response = await fetch(`/users/username/${username}`);
                 const userData = await response.json();
                 await setLoggedUser(userData);
                 console.log('User successfully logged in.');
@@ -46,17 +46,10 @@ function LoginPage({loggedUser, setLoggedUser}){
     return (
          <Container className='d-flex justify-content-center align-items-center custom-container text-white' style={{ minHeight: '100vh' }}>
             {loggedUser ? (
-                // Show only when user is logged in
                 <>
-                    <h2>Welcome, {username}</h2>                    
-                    <div className="cancel-button">
-                        <Link to="/">
-                            <button>Home</button>
-                        </Link>
-                    </div>
+                    <h2>Welcome, {username}</h2>                 
                 </>
             ) : (
-                // Show only when user is not logged in
                 <Form>
                     <Row className='justify-content-center text-center'>
                         <Col xs={12} md={12} lg={12}>
