66e0db4d903e59302d36c5e242035731dcebf9af
GaborToth22 <toccs22@gmail.com>
2024-04-24 12:58:38 +0200

    feat: add proxy to vite.config

--- TBOTFrontend/vite.config.js
+++ TBOTFrontend/vite.config.js
@@ -1,7 +1,47 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+
+  // eslint-disable-next-line no-undef
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      server: { https: true },
+      watch: {
+        usePolling: true,
+      },
+      host: true, 
+      proxy: {
+        '/auth': {
+          target: env.BACKEND_URL || 'http://localhost:5263/',
+          changeOrigin: true,
+          credentials: true,
+        },
+        '/users': {
+          target: env.BACKEND_URL || 'http://localhost:5263/',
+          changeOrigin: true,
+          credentials: true,
+        },
+        '/balance': {
+          target: env.BACKEND_URL || 'http://localhost:5263/',
+          changeOrigin: true,
+          credentials: true,
+        },
+        '/expense': {
+          target: env.BACKEND_URL || 'http://localhost:5263/',
+          changeOrigin: true,
+          credentials: true,
+        },
+        '/friendship': {
+          target: env.BACKEND_URL || 'http://localhost:5263/',
+          changeOrigin: true,
+          credentials: true,
+        },
+      },
+    },
+  };
+});
