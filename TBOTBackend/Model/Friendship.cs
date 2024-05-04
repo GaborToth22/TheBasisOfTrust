2adae6692e5c0c8525e5c17a250501a3791fd1f4
GaborToth22 <toccs22@gmail.com>
2024-05-04 09:41:07 +0200

    refactor: Friendship.cs

--- TBOTBackend/Model/Friendship.cs
+++ TBOTBackend/Model/Friendship.cs
@@ -2,10 +2,14 @@ namespace TBOTBackend.Model;
 
 public class Friendship
 {
-    public int Id { get; init; }
-    public int UserId { get; init; }
-    public int FriendId { get; init; }
-    public User User { get; set; }
-    public User Friend { get; set; }
+    public int Id { get; set; }
+    public int SenderId { get; set; }
+    public string SenderName { get; set; }
+    public string SenderEmail { get; set; }
+    public User Sender { get; set; }
+    public int ReceiverId { get; set; }
+    public string ReceiverName { get; set; }
+    public string ReceiverEmail { get; set; }
+    public User Receiver { get; set; }
     public bool Accepted { get; set; }
 }
