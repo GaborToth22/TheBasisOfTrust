f0f89ef2318e03735adf7b8f09ab23a7e5fc6a29
GaborToth22 <toccs22@gmail.com>
2024-05-04 09:38:25 +0200

    refactor: FriendshipsSent and FriendshipsReceived fields

--- TBOTBackend/Model/User.cs
+++ TBOTBackend/Model/User.cs
@@ -7,5 +7,6 @@ public class User
     public int Id { get; init; }
     public string Username { get; set; }
     public string Email { get; set; }
-    public ICollection<Friendship> Friendships { get; set; }
+    public ICollection<Friendship> FriendshipsSent { get; set; }
+    public ICollection<Friendship> FriendshipsReceived { get; set; }
 }
