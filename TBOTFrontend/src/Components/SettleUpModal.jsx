f45b98ee83f568832b289a2097b5f44d2402af2c
GaborToth22 <toccs22@gmail.com>
2024-05-09 16:43:53 +0200

    feat: add SettleUpModal and implement it to DashboardPage.jsx

---
+++ TBOTFrontend/src/Components/SettleUpModal.jsx
@@ -0,0 +1,188 @@
+import {Modal, Button, Form, FormGroup, FormLabel, Row, Col  } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { useLoggedUser } from '../Services/LoggedUserProvider';
+import DeleteFriend from './DeleteFriend';
+import AddFriend from './AddFriend';
+
+function SettleUpModal(props) {
+  const { loggedUser, setLoggedUser } = useLoggedUser();
+  const { userBalances } = props;
+  const [selectedFriend, setSelectedFriend] = useState(null);    
+  const [amount, setAmount] = useState(null);
+  const [date, setDate] = useState(new Date().toISOString().substr(0, 10));
+  const [paidBy, setPaidBy] = useState(loggedUser.id);
+  const [message, setMessage] = useState("");
+
+  
+console.log(userBalances)
+  const handleFilterChange = (event) => {
+    setSelectedFriend(event.target.value);
+  };
+
+  const handlePaidByChange = (event) => {
+    if(event.target.value == loggedUser.id && selectedFriend == loggedUser.id){
+      setSelectedFriend(null)
+    }else if(event.target.value !== loggedUser.id){
+      setSelectedFriend(loggedUser.id)
+    }
+    setPaidBy(event.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const participantIds = [];
+      const description = "Settle Up"
+      participantIds.push(paidBy);
+      participantIds.push(selectedFriend);
+      const split = 3
+      if(paidBy == loggedUser.id){
+        2
+      }
+      const response = await fetch('/expense', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          participantIds: participantIds,
+          amount: amount,
+          date: date,
+          description: description,
+          paidById: paidBy,
+          split: split                 
+        }),
+      });
+
+      if (response.ok) {
+        console.log('Expense successfully created.');
+        setMessage('Settle Up successfully');
+        setTimeout(() => {
+          handleClose();
+        }, 1500);
+      } else {
+        console.error('Failed to create expense.');
+        setMessage('Something went wrong, check that you have filled in everything');
+      }
+    } catch (error) {
+      console.error('Failed to create expense.');
+      setMessage('Something went wrong, check that you have filled in everything');
+    }
+  }
+
+  const handleClose = () => {    
+    setDate(new Date().toISOString().substr(0, 10));
+    setSelectedFriend(null);
+    setAmount(null);
+    setPaidBy(loggedUser.id);    
+    setMessage("");
+    props.onHide();
+};  
+
+  function createOptionsToPay(userBalances){
+    const friendships = loggedUser.friendshipsSent.concat(loggedUser.friendshipsReceived);
+    return(
+      <>
+          {Object.entries(userBalances).map(([userId, balance]) => {
+              if (balance < 0) {
+                  const friend = friendships.find(friendship => friendship.receiverId == userId || friendship.senderId == userId);
+                  const friendName = friend.receiverId == userId ? friend.receiverName : friend.senderName;
+                  const id = friend.receiverId == userId ? friend.receiverId : friend.senderId;
+                  return (
+                      <option key={friend.id} value={id}>{friendName}</option>
+                  );
+              } else {
+                  return null;
+              }
+          })}
+      </>
+      )
+  }
+
+  function createOptionsToGet(userBalances){
+    const friendships = loggedUser.friendshipsSent.concat(loggedUser.friendshipsReceived);
+    return(
+      <>
+          {Object.entries(userBalances).map(([userId, balance]) => {
+              if (balance > 0) {
+                  const friend = friendships.find(friendship => friendship.receiverId == userId || friendship.senderId == userId);
+                  const friendName = friend.receiverId == userId ? friend.receiverName : friend.senderName;
+                  const id = friend.receiverId == userId ? friend.receiverId : friend.senderId;
+                  return (
+                      <option key={friend.id} value={id}>{friendName}</option>
+                  );
+              } else {
+                  return null;
+              }
+          })}
+      </>
+      )
+  }
+
+  console.log(paidBy)
+  console.log(selectedFriend)
+  return (
+    <Modal
+      {...props}
+      size="md"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      onHide={() => {        
+        setDate(new Date().toISOString().substr(0, 10));
+        setSelectedFriend(null);
+        setAmount(null);
+        setPaidBy(loggedUser.id);
+        setMessage("");
+        props.onHide();
+      }}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Settle Up
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+       <Form>
+        <Row>
+          <Col className="col-auto mt-1">
+              <Form.Control as="select" value={paidBy} onChange={handlePaidByChange}>
+                <option value={loggedUser.id}>You</option>  
+                {createOptionsToGet(userBalances)}                
+              </Form.Control>
+          </Col>
+          <Col className="col-auto mt-2">Paid</Col>              
+          <Col className="col-auto mt-1">
+              <Form.Control as="select" value={selectedFriend} onChange={handleFilterChange}>
+                <option value={null}>Select a friend</option>                 
+                {paidBy == loggedUser.id ? createOptionsToPay(userBalances) : <option value={loggedUser.id}>You</option>}              
+              </Form.Control>
+          </Col>
+        </Row>     
+          <FormGroup>
+            <FormLabel>Amount</FormLabel>
+            <Form.Control type="number" placeholder="0.00" value={amount} onChange={(e) => setAmount(e.target.value)}/>
+          </FormGroup>          
+          <FormGroup>
+            <FormLabel>Date</FormLabel>
+            <Form.Control type="date" value={date} onChange={(e) => setDate(e.target.value)}/>
+          </FormGroup>                
+          </Form>
+            <div>{message}</div>
+        </Modal.Body>
+        <Modal.Footer className="d-flex justify-content-between align-items-center">
+          <div>
+            <img src='/src/Images/TBOT.png' alt='TBOTimg' className='tbot-image'/>
+          </div>
+          <div>
+            <Button onClick={handleSubmit}>Submit</Button>                
+          </div>
+          <div>
+          <Button onClick={handleClose}>Close</Button>
+          </div>
+        </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default SettleUpModal;
--- TBOTFrontend/src/Pages/DashboardPage.jsx
+++ TBOTFrontend/src/Pages/DashboardPage.jsx
@@ -6,6 +6,8 @@ import AddFriend from "../Components/AddFriend";
 import DeleteFriend from "../Components/DeleteFriend";
 import AddFriendModal from "../Components/AddFriendModal";
 import AcceptRequest from "../Components/AcceptRequest";
+import AddExpenseModal from "../Components/AddExpenseModal";
+import SettleUpModal from "../Components/SettleUpModal";
 
 
 function DashboardPage(){  
@@ -15,11 +17,16 @@ function DashboardPage(){
     const [filteredFriends, setFilteredFriends] = useState([]);
     const [addFriendModalShow, setAddFriendModalShow] = useState(false);
     const [addExpenseModalShow, setAddExpenseModalShow] = useState(false);
+    const [settleUpModalShow, setSettleUpModalShow] = useState(false);
+    const [youOwe, setYouOwe] = useState(0);
+    const [total, setTotal] = useState(0);
+    const [owesYou, setOwesYou] = useState(0);
+    const [userBalances, setUserBalances] = useState({});
 
 
     useEffect(() => {   
-        fetchBalances();
-    }, [loggedUser]);
+        fetchBalances();        
+    }, [loggedUser, addExpenseModalShow, settleUpModalShow]);
 
     useEffect(() => {
         if (loggedUser) {
@@ -32,6 +39,41 @@ function DashboardPage(){
             setFilteredFriends(filtered);
         }
     }, [loggedUser, filter]);
+
+    useEffect(() => {
+        if (balances && loggedUser) {
+            const userBalances = {};
+            let youOweSum = 0.00;
+            let owesYouSum = 0.00;
+
+            balances.forEach(balance => {
+                const amount = balance.amount;                
+                console.log(balance.userId)
+                if (balance.userId == loggedUser.id) {
+                    userBalances[balance.participantUserId] = (userBalances[balance.participantUserId] || 0) + amount;
+                } else {
+                    userBalances[balance.userId] = (userBalances[balance.userId] || 0) - amount;
+                }})
+
+            Object.values(userBalances).forEach(balance => {
+                if (balance < 0) {
+                    youOweSum += balance;
+                } else {
+                    owesYouSum += balance;
+                }
+            });
+    console.log(youOweSum)
+            const formattedBalances = {};
+        Object.keys(userBalances).forEach(key => {
+            formattedBalances[key] = userBalances[key].toFixed(2);
+        });
+
+        setYouOwe(youOweSum.toFixed(2));
+        setOwesYou(owesYouSum.toFixed(2));
+        setTotal((owesYouSum + youOweSum).toFixed(2));
+        setUserBalances(formattedBalances);
+        }
+    }, [balances, loggedUser, addExpenseModalShow]);
 
     const fetchBalances = async () => {
         try {
@@ -63,6 +105,48 @@ function DashboardPage(){
         setFilter(event.target.value);
     };
 
+    function renderYouOwe(userBalances) {
+      const friendships = loggedUser.friendshipsSent.concat(loggedUser.friendshipsReceived);
+      return(
+        <>
+            {Object.entries(userBalances).map(([userId, balance]) => {
+                if (balance < 0) {
+                    const friend = friendships.find(friendship => friendship.receiverId == userId || friendship.senderId == userId);
+                    const friendName = friend.receiverId == userId ? friend.receiverName : friend.senderName;
+                    return (
+                        <div key={userId}>
+                            <span>{friendName} </span><span style={{ color: '#Fbb090' }}>{balance}</span>                            
+                        </div>
+                    );
+                } else {
+                    return null;
+                }
+            })}
+        </>
+      )        
+    }
+
+    function renderOwesYou(userBalances) {
+      const friendships = loggedUser.friendshipsSent.concat(loggedUser.friendshipsReceived);
+      return(
+        <>
+            {Object.entries(userBalances).map(([userId, balance]) => {
+                if (balance > 0) {
+                    const friend = friendships.find(friendship => friendship.receiverId == userId || friendship.senderId == userId);
+                    const friendName = friend.receiverId == userId ? friend.receiverName : friend.senderName;
+                    return (
+                        <div key={userId}>
+                            <span>{friendName} </span><span className='fontsGreen'>{balance}</span>                            
+                        </div>
+                    );
+                } else {
+                    return null;
+                }
+            })}
+        </>
+      )        
+    }
+
     function renderFriends(loggedUser) {
         
         return (
@@ -70,7 +154,7 @@ function DashboardPage(){
                 {filteredFriends.map(friendship => (
                     <div key={friendship.id} className="d-flex justify-content-between align-items-center" style={{ margin: '1rem' ,borderBottom: '1px solid black' }}>
                         <div>{friendship.receiverName === null ? friendship.senderName : friendship.receiverName}</div>                    
-                        <DeleteFriend onClick={() => deleteFriend(loggedUser.id, friendship.receiverName === null ? friendship.senderId : friendship.receiverId)}/>
+                        <DeleteFriend text={"Delete Friendship"} onClick={() => deleteFriend(loggedUser.id, friendship.receiverName === null ? friendship.senderId : friendship.receiverId)}/>
                     </div>
                 ))}
             </>
@@ -86,7 +170,7 @@ function DashboardPage(){
                 <div>{request.senderName}</div>
                 <div>
                     <AcceptRequest onClick={() => acceptFriend(loggedUser.id, request.senderId)}/>
-                    <DeleteFriend onClick={() => deleteFriend(loggedUser.id, request.senderId)}/>
+                    <DeleteFriend text={"Decline request"} onClick={() => deleteFriend(loggedUser.id, request.senderId)}/>
                 </div>
             </div>
         ));
@@ -96,7 +180,7 @@ function DashboardPage(){
         .map(request => (
             <div key={request.id} className="d-flex justify-content-between align-items-center" style={{ margin: '1rem', borderBottom: '1px solid black' }}>
                 <div>{request.receiverName}</div>
-                <DeleteFriend onClick={() => deleteFriend(loggedUser.id, request.receiverId)}/>
+                <DeleteFriend text={"Decline request"} onClick={() => deleteFriend(loggedUser.id, request.receiverId)}/>
             </div>
         ));
             
@@ -151,9 +235,10 @@ function DashboardPage(){
             console.error('Error fetching delete data:', error);
         }
     };
-    
+   
 
     console.log(balances);
+    console.log(userBalances);
     console.log(loggedUser);
     
     return (
@@ -168,7 +253,7 @@ function DashboardPage(){
                     </FormGroup>
                     <div className="mt-3" style={{backgroundColor: '#067f99', borderRadius: '6px'}}>
                         <div style={{backgroundColor: '#067f99', borderRadius: '6px' , height: '91vh'}}>
-                            <div className="d-flex justify-content-between "><div className='ml-3'>Friends</div><AddFriend onClick={() => setAddFriendModalShow(true)}/></div>
+                            <div className="d-flex justify-content-between "><div className='ml-3'>Friends</div><AddFriend text={"+Add Friend"} onClick={() => setAddFriendModalShow(true)}/></div>
                             {renderFriends(loggedUser)}  
                         </div> 
                     </div> 
@@ -177,24 +262,24 @@ function DashboardPage(){
                     <Container style={{backgroundColor: '#067f99', borderRadius: '6px', border: '1px solid black'}}>
                         <Row>
                             <Col className="d-flex justify-content-between align-items-center">
-                                <Button variant="warning" style={{width: '33%'}}>Add Expense</Button>
+                                <Button variant="warning" onClick={() => setAddExpenseModalShow(true)} style={{width: '33%'}}>Add Expense</Button>
                                 <img src='/src/Images/TBOT.png' alt='TBOTimg' className='tbot-image'/>
-                                <Button variant="success" style={{width: '33%'}}>Settle Up</Button>
+                                <Button variant="success" onClick={() => setSettleUpModalShow(true)} style={{width: '33%'}}>Settle Up</Button>
                             </Col>
                         </Row>
                         <Row>
                             <Col className="d-flex justify-content-between align-items-center" style={{borderTop: '1px solid black', borderBottom: '1px solid black'}}>
                                 <div className="align-items-center justify-content-center text-center" style={{width: '33%'}}>
                                     <p>You owe</p>
-                                    <p className="fonts">15€</p>
+                                    <p className={youOwe !== 0 ? "fontsRed" : "fonts"}>{youOwe} </p>
                                 </div>
                                 <div className="align-items-center justify-content-center text-center" style={{width: '33%'}}>
                                     <p>Total balance</p>
-                                    <p className="fonts">1555€</p>
+                                    <p className={total > 0 ? "fontsGreen" : (total < 0 ? "fontsRed" : "fonts")}>{total}</p>
                                 </div>
                                 <div className="align-items-center justify-content-center text-center" style={{width: '33%'}}>
                                     <p>You are owed</p>
-                                    <p className="fonts">15€</p>
+                                    <p className={owesYou !== 0 ? "fontsGreen" : "fonts"}>{owesYou}</p>
                                 </div>
                             </Col>
                         </Row>
@@ -202,9 +287,11 @@ function DashboardPage(){
                             <Col className="d-flex justify-content-between align-items-center fonts">
                                 <div style={{width: '50%', height: '80vh'}} >
                                     <p>You Owe</p>
+                                    {renderYouOwe(userBalances)}
                                 </div>
                                 <div className="text-end" style={{width: '50%', height: '80vh'}}>
                                     <p>You are owed</p>
+                                    {renderOwesYou(userBalances)}
                                 </div>
                             </Col>
                         </Row>
@@ -213,13 +300,15 @@ function DashboardPage(){
                 <Col xs={3} md={3} lg={3} style={{ marginTop: '2.3rem' }}>
                 <div className="mt-3" style={{backgroundColor: '#067f99', borderRadius: '6px'}}>
                         <div style={{backgroundColor: '#067f99', borderRadius: '6px' , height: '91vh'}}>
-                            <div className="d-flex justify-content-between "><div className='ml-3'>Friend Requests</div><AddFriend onClick={() => setAddFriendModalShow(true)}/></div>
+                            <div className="d-flex justify-content-between "><div className='ml-3'>Friend Requests</div><AddFriend text={"+Add Friend"} onClick={() => setAddFriendModalShow(true)}/></div>
                             {renderFriendRequestes(loggedUser)}  
                         </div>
                     </div>                    
                 </Col>
             </Row>
             <AddFriendModal show={addFriendModalShow} fetchUser={fetchUser} onHide={() => setAddFriendModalShow(false)}/>
+            <AddExpenseModal show={addExpenseModalShow} fetchUser={fetchUser} onHide={() => setAddExpenseModalShow(false)}/>
+            <SettleUpModal show={settleUpModalShow} userBalances={userBalances} fetchUser={fetchUser} onHide={() => setSettleUpModalShow(false)}/>
         </Container>
         </>
     )
