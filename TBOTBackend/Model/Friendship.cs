0eba179814b994c7982429bc897f8cb6dab69c41
GaborToth22 <toccs22@gmail.com>
2024-05-15 08:26:49 +0200

    feat: add new propertys to Friendship.cs

--- TBOTBackend/Model/Friendship.cs
+++ TBOTBackend/Model/Friendship.cs
@@ -4,6 +4,10 @@ public class Friendship
 {
     public int Id { get; set; }
     public int SenderId { get; set; }
+    public string SenderName { get; set; }
+    public string SenderEmail { get; set; }
     public int ReceiverId { get; set; }
+    public string ReceiverName { get; set; }
+    public string ReceiverEmail { get; set; }
     public bool Accepted { get; set; }
 }
